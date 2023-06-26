12768c4fe228b19f5cb39dffed3a4a990ecf8b3c
Jorge Alan <jorge.alann@hotmail.com>
2023-06-26 07:41:47 -0300

    Envio de email confirmação de cadastro

---
+++ update/v1.2.2/app/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav>
+        <router-link to="/">Dashboard</router-link>        
+    </nav>
+</template>
+
+<script>
+ export default{
+
+ }
+</script>
+
+<style scoped>
+
+    nav {
+    padding: 30px;
+    }
+
+    nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    }
+
+    nav a.router-link-exact-active {
+    color: #42b983;
+    }
+
+
+</style>
